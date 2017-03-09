5e802ca974d896a23c2908e863e80bf0d5ea48f5
isoletsy <xielingyun@linuxdeepin.com>
2017-03-09 11:33:59 +0800

    fixed parse repos  from p13

--- abuilder.py
+++ abuilder.py
@@ -1,24 +1,56 @@
 #!python3
 
 import argparse
+import os
+import sys
 
+### global variables ###
+upload_list = []
+
+
+### flags ###
+BEFORE_BUILD = 0
+AFTER_BUILD = 0
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Build packages in one build environment')
+        description='Build packages depend on params')
 
     parser.add_argument('-r', '--repo', metavar='repos', required=True,
                         dest='repos', action='append',
                         help='repo(s) to be updated after this build')
 
+    parser.add_argument('-s', '--source', metavar='source', required=True,
+                        dest='source', help='source code git address')
+
     args = parser.parse_args()
+
+    # set the flags
+    BEFORE_BUILD = 1
+    AFTER_BUILD = 0
+
     # Use the collected arguments
-    dputto(args.repos)
+    set_upload_repos(args.repos)
 
 
-def dputto(repos):
-    for item in repos:
-        print(item)
+def set_upload_repos(repos):
+    if BEFORE_BUILD and not upload_list and repos:
+        for item in repos:
+            upload_list.append(item)
+    else:
+        print("Initial upload repos failed")
+        sys.exit(1)
+
+
+def upload_debs():
+    # only upload packages after the build process
+    if AFTER_BUILD and not upload_list:
+        for item in upload_list:
+            os.system("dput -uf " + item + "../*.changes")
+    else:
+        pass
+
 
 if __name__ == '__main__':
     main()
+
