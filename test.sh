731fe0d5252660df14f4b6441951939788a5dcf1
isoletsy <xielingyun@linuxdeepin.com>
2017-07-19 18:03:16 +0800

    Auto build the missing build depends and support packages' name in build list

--- test.sh
+++ test.sh
@@ -2,9 +2,6 @@
 
 # TODO: use pbuilder check build depends
 
-# all path in this script shuould be complete path
-INITSCRIPT="initbuild"
-
 BUILD_PLACE="/dev/shm/rebuild"
 BUILD_IN_MEMORY=0
 
@@ -16,16 +13,30 @@ X86_SWITCH=0
 # build failed flag, ignore this package
 FAILED_FLAG=0
 
-RESULT_DIR="/home/deepin/pbuilder-results"
-RESULT_BACKUP_DIR="/home/deepin/pbuilder-results-backup"
+# enable pacakges' name build
+SOURCE_NAME=''
 
-REPOS="/home/deepin/rebuild-repos"
+RESULT_DIR="$HOME/pbuilder-results"
+RESULT_BACKUP_DIR="$HOME/pbuilder-results-backup"
+
+REPOS="$HOME/rebuild-repos"
 # record the *_all.deb, which should not been built
 RECORD_LIST_FILE="checked_all.list"
 
 BUILD_TMP_DIR="/tmp/rebuild"
 
 BUILD_LIST=""
+TMP_BUILD_LIST=""
+
+# path of scripts
+# all path in this script shuould be complete path
+INITSCRIPT="$HOME/rebuild-scripts/initbuild"
+SCRIPT_PATH="$HOME/rebuild-scripts/test.sh"
+if [[ ! -f ${SCRIPT_PATH} ]]; then
+	echo "Check the script path failed."
+	echo "Set SCRIPT_PATH in this script correctly."
+	exit 9
+fi
 
 # use this to check the build depends
 BACKUP_CHROOT="/var/cache/pbuilder/build/check"
@@ -99,16 +110,16 @@ prepare_source()
 
 	# get source from x86 directly
 	if [[ ${X86_SWITCH} -eq 1 ]]; then
-		get_source_x86 $1
+		get_source_x86 ${SOURCE_NAME}
 	else
 		sudo apt update
-		apt source $1
+		apt source ${SOURCE_NAME}
 		
 		dsc_file=$(ls ./ | grep 'dsc')
 		if [[ x${dsc_file} == "x" ]]; then
 			# failed to get package source from arch repos
 			# try x86 repo
-			get_source_x86 $1
+			get_source_x86 ${SOURCE_NAME}
 		fi
 	fi
 
@@ -121,19 +132,28 @@ prepare_source()
 		return 1
 	else
 		# check the dsc name
-		dsc_name=$(cat ${dsc_file} | awk -F'_' '{print $1}')
+		dsc_name=$(echo ${dsc_file} | awk -F'_' '{print $1}')
 		# ${line} in ${BUILD_LIST} is a source name
 		if [[ x${dsc_name} == x${SOURCE_NAME} ]]; then
 			#statements
-			echo "Get source: ${dsc_name}"
+			echo "SOURCE_NAME is ${dsc_name}"
 		else
 			# ${line} in ${BUILD_LIST} is a package name
+			echo "Get source ${dsc_name} from ${SOURCE_NAME}"
+			echo "Set SOURCE_NAME to ${dsc_name}"
+			# mv the tobuild dir name from package name to source name
+			if [[ -d ${dsc_name} ]]; then
+				# sometimes failed build source exists in ${BUILD_TMP_DIR}, remove it
+				rm -rf ${dsc_name}
+			fi
+			mv -v ${SOURCE_NAME} ${dsc_name}
+			
 			SOURCE_NAME=${dsc_name}
 		fi
 	fi
 
 	# get source successfully
-	update_version $1
+	update_version ${SOURCE_NAME}
 	return 0
 
 }
@@ -184,27 +204,66 @@ run_pbuilder()
 	# 
 	if [[ ${BUILD_IN_MEMORY} -eq 1 ]]; then
 		# use shm to build 
-		sudo pbuilder --build --basetgz ${BASE_TGZ} --buildplace ${BUILD_PLACE} --buildresult ${RESULT_DIR} --hookdir /var/cache/pbuilder/hooks/ --logfile ${RESULT_DIR}/buildlog --debbuildopts -sa $1/$1*.dsc
+		sudo pbuilder --build --use-network yes --basetgz ${BASE_TGZ} --buildplace ${BUILD_PLACE} --buildresult ${RESULT_DIR} --hookdir /var/cache/pbuilder/hooks/ --logfile ${RESULT_DIR}/buildlog --debbuildopts -sa $1/$1*.dsc
 	else
-		sudo pbuilder --build --basetgz ${BASE_TGZ} --buildresult ${RESULT_DIR} --hookdir /var/cache/pbuilder/hooks/ --logfile ${RESULT_DIR}/buildlog --debbuildopts -sa $1/$1*.dsc
+		sudo pbuilder --build --use-network yes --basetgz ${BASE_TGZ} --buildresult ${RESULT_DIR} --hookdir /var/cache/pbuilder/hooks/ --logfile ${RESULT_DIR}/buildlog --debbuildopts -sa $1/$1*.dsc
 	fi
 
 	if [[ $? -ne 0 ]]; then
 		# failed to build
 		echo "Failed to build $1."
-		# TODO: check the error, rebuild it or not
+		
+		iter_build_depends
+
+		# after the function iter_build_depends, ${SOURCE_NAME} should be built
+		# this clean_build may be failed
+		clean_build $1
 
 		# now, if build failed in pbuilder, run the C10shell in pbuilder
-		clean_build $1
+		# TODO: if the depends is a arch independent, the script will run in a loop
 		exit 1
 	fi
 }
 
+iter_build_depends()
+{
+	build_error_log=${RESULT_DIR}/buildlog
+	# generate new BUILD_LIST
+	TMP_BUILD_LIST="/tmp/tmpbuild-$(date +%s)"
+	# examples:
+	# pbuilder-satisfydepends-dummy : Depends: erlang-base which is a virtual package and is not provided by any available package.
+    #                             Depends: php5 which is a virtual package and is not provided by any available package.
+    #                             Depends: php5-dev which is a virtual package and is not provided by any available package.
+	grep 'which is a virtual package' ${build_error_log} |  sed 's/.*Depends: //g' | awk '{print $1}' > ${TMP_BUILD_LIST}
+
+	# build the depends
+	# ${TMP_BUILD_LIST} should be exist
+	if [[ ! -f ${TMP_BUILD_LIST}  ]]; then
+		echo "Failed to check the depends for ${SOURCE_NAME}"
+		exit 2
+	fi
+	if [[ $(stat -t ${TMP_BUILD_LIST} | awk '{print $2}') -eq 0 ]]; then
+		# TMP_BUILD_LIST is empty
+		echo "Generate the depends for ${SOURCE_NAME} failed"
+		exit 3
+	else
+		# TODO: disable
+		# add this failed build to TMP_BUILD_LIST
+		# echo ${SOURCE_NAME} >> ${TMP_BUILD_LIST}
+		bash ${SCRIPT_PATH} ${TMP_BUILD_LIST}
+	fi
+}
+
+
 clean_build()
 {
-	rm -rf ${BUILD_TMP_DIR}/$1
+	# clean $(apt source xxx) files
+	if [[ -d ${BUILD_TMP_DIR}/${SOURCE_NAME} ]]; then
+		rm -rf ${BUILD_TMP_DIR}/${SOURCE_NAME}
+	fi
+	
 	# reset RESULT_DIR
-	RESULT_DIR="/home/deepin/pbuilder-results"
+	RESULT_DIR="$HOME/pbuilder-results"
 }
 
 reprepro_include()
@@ -260,9 +319,10 @@ build_pass()
 {
 	# only grep the word $1, not *$1*, *$1-* and *-$1*
 	# TODO: $1-$1 is still in this grep result
-	search_pack=$(reprepro -b ${REPOS} list unstable | grep source | grep -w $1 | awk '{print $2}' | grep $1$ | grep ^$1 )
+	search_source=$(reprepro -b ${REPOS} list unstable | grep source: | grep -w $1 | awk '{print $2}' | grep $1$ | grep ^$1 )
+	search_pack=$(reprepro -b ${REPOS} list unstable | grep mips64el: | grep -w $1 | awk '{print $2}' | grep $1$ | grep ^$1 )
 
-	if [[ ${search_pack} == $1 ]]; then
+	if [[ ${search_source} == $1 || ${search_pack} == $1 ]]; then
 		# package had been built 
 		# return 0 to pass this build
 		return 0
@@ -283,12 +343,16 @@ build_pass()
 split_line()
 {
 	# $1 must be a package name
-	line=$1
+	SOURCE_NAME=$1
 	# $2 is a flag
 	if [[ x$2 == "xx86" ]]; then
 		X86_SWITCH=1
 	elif [[ x$2 == "xfailed" ]]; then
 		FAILED_FLAG=1
+	# elif [[ x$2 == "xsource" ]]; then
+	# 	SOURCE_FLAG=1
+	# 	# set get source name, used in apt source
+	# 	GET_SOURCE_NAME=$3
 	fi
 }
 
@@ -321,38 +385,45 @@ main()
 	fi
 
 	while read -r line; do
+		# if $line is a blank line, ignore it
+		if [[ x${line} == 'x' ]]; then
+			continue
+		fi
 		# reset X86_SWITCH
 		X86_SWITCH=0
 		# reset BUILD_IN_MEMORY
 		BUILD_IN_MEMORY=0
 
 		# special format in $line, split it to normal package name
+		# set the ${SOURCE_NAME}
 		# split_line example: split_line aaa x86
-		split_line $line
+		split_line ${line}
+
 
 		if [[ ${FAILED_FLAG} -eq 1 ]]; then
 			# build failed before
-			echo "${line} cann't build."
+			echo "${SOURCE_NAME} cann't build."
 			# reset FAILED_FLAG
 			FAILED_FLAG=0
 			continue
 		fi
 
-		if build_pass ${line}; then
-			# if ${line} is in repo, pass this build
-			echo "${line} had been built."
+		if build_pass ${SOURCE_NAME}; then
+			# if ${SOURCE_NAME} is in repo, pass this build
+			echo "${SOURCE_NAME} had been built."
 			# remove it from ${BUILD_LIST}
-			remove_passed_package ${line}
+			remove_passed_package ${SOURCE_NAME}
 			continue
 		fi
 
-		if prepare_source ${line}; then
-			if check_all ${line}; then
-				rebuild ${line}
+		if prepare_source ${SOURCE_NAME}; then
+			# ${SOURCE_NAME} may be reset in prepare_source and different from ${SOURCE_NAME} 
+			if check_all ${SOURCE_NAME}; then
+				rebuild ${SOURCE_NAME}
 			else
-				echo ${line} "is arch independent. Build pass."
+				echo ${SOURCE_NAME} "is arch independent. Build pass."
 				# remove it from ${BUILD_LIST}
-				remove_passed_package ${line}
+				remove_passed_package ${SOURCE_NAME}
 				continue
 			fi
 		fi
